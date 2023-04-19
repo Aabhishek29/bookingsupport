c9eda292b5d9e6a8e9191489aceae0adf4074e99
Aabhishek29 <starkabhishek29@gmail.com>
2023-04-19 20:28:47 +0530

    first commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashBoard = () => {
+  return(
+		<div>
+			<header>Hello world</header>
+		</div>
+  )
+}
+
+export default DashBoard;
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = () => {
+  return(
+		<div>
+			<header>Login</header>
+		</div>
+	)
+}
+export default Login;

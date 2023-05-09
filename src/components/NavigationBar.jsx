4656e1ce29d08fbd048f225d924dc5424061178c
Aabhishek29 <starkabhishek29@gmail.com>
2023-05-09 20:32:23 +0530

    contact us update

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -4,32 +4,47 @@ import './style/navigation.css';
 import { BsList } from 'react-icons/bs';
 // import Logo from '../assets/favicon.ico';
 
-const NavigationBar = () => {
-  return(
-	  <div className={'navigation'}>
-		<div className="nav-bar">
-			{/*<img id={'navigation-icon'} src={Logo} alt={'something went wrong'}/>*/}
-			<header className="navigation-title">Bookingsupport.in</header>
-			<ul className="navigation-bar">
-				<li className="nav-item">
-					<Link className="navigation-link" to="/">Home</Link>
-				</li>
-				<li className="nav-item">
-					<Link className="navigation-link" to="/about">About us</Link>
-				</li>	
-				<li className="nav-item">
-					<Link className="navigation-link" to="/faq">FAQ</Link>
-				</li>
-				<li className="nav-item">
-					<Link className="navigation-link" to="/contact">Contact us</Link>
-				</li>
-			</ul>
-		</div>
-		  <div className={"navigation-bar-list"}>
-			  <BsList fontSize={24} />
-		  </div>
-	  </div>
-	)
+class NavigationBar extends React.Component{
+	constructor() {
+		super();
+		this.state = {
+			isDropDown: window.innerWidth > 900,
+			width: window.innerWidth,
+		}
+	}
+	showDropDown = () => {
+		this.setState({ isDropDown: !this.state.isDropDown});
+	}
+
+	render() {
+		return (
+			<div className={'navigation'}>
+				<div className="nav-bar">
+					{/*<img id={'navigation-icon'} src={Logo} alt={'something went wrong'}/>*/}
+					<header className="navigation-title">Bookingsupport.in</header>
+					{this.state.isDropDown && (
+						<ul className="navigation-bar">
+							<li className="nav-item">
+								<Link className="navigation-link" to="/">Home</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="navigation-link" to="/about">About us</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="navigation-link" to="/faq">FAQ</Link>
+							</li>
+							<li className="nav-item">
+								<Link className="navigation-link" to="/contact">Contact us</Link>
+							</li>
+						</ul>
+					)}
+				</div>
+				<div className={"navigation-bar-list"}>
+					<BsList onClick={this.showDropDown} fontSize={24}/>
+				</div>
+			</div>
+		)
+	}
 }
 
 export default NavigationBar;
--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { useState } from 'react';
 import './style/Contact.css';
+import Icon from "../assets/iiicon.png";
+import Logo from "../assets/faq.png";
 
 const Contact = () => {
     const [name, setName] = useState("");
@@ -15,6 +17,24 @@ const Contact = () => {
 
     return(
         <div className={'contact'}>
+            <div className={'faq-introduction-div'}>
+                <header className={'faq-introduction-div-header'}>Send BookingSupport.in a Message</header>
+                <div className={'faq-section'}>
+                    <div className={'faq-introduction-div-description'}>
+                        <label className={'faq-introduction-div-description-text'}>
+                            You’re welcome to shoot us a question about anything at all. Before you write, please
+                            see FAQs if you have any question regading our services. It will help you find almost
+                            every answer you want from us. Please, click on FAQ for your queries.
+                        </label>
+                        <div className={'image-icon-section'}>
+                            <img className={'image-icon'} src={Icon} alt={'something went wrong'}/>
+                        </div>
+                    </div>
+                    <div className={'image-section'}>
+                        <img className={'image-logo'} src={Logo} alt={'something went wrong'}/>
+                    </div>
+                </div>
+            </div>
             <div className={'contact-us-header'}>
                 <header>Ask Anything</header>
             </div>
